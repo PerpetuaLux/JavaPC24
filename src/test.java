ee207534a059bf3a59b53a8941960864192bd538
Jonathan <>
2017-11-27 04:44:30 +0000

    testing the thing

--- src/test.java
+++ src/test.java
@@ -3,6 +3,7 @@ class test{
     public static void main(String[] args) {
         System.out.println("My speed is " + speed + ", my age is " + age 
                 + " and the time is " + time);
+        //testing the thing
     }
     
 }
