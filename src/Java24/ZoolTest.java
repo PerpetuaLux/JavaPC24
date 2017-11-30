46e7853507300e656f5fb9ec0e5fb9e7030ffdb0
Jonathan <>
2017-11-30 21:55:23 +0000

    Working on exercises

---
+++ src/Java24/ZoolTest.java
@@ -0,0 +1,20 @@
+package Java24;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jonathan
+ */
+public class ZoolTest {
+    public static void zool(int eleven, String pet, String street) {
+        System.out.printf("You typed %d, %s, %s.\n", eleven, pet, street);
+    }
+    public static void main(String[] args) {
+        zool(11, "Jenny", "Elm Drive");
+    }
+}
