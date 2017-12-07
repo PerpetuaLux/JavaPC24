991391cda74a1e69915f94a5cb39d7670372be0d
Jonathan <>
2017-12-07 15:01:36 +0000

    messing about with Pi

---
+++ src/Java24/PiTest.java
@@ -0,0 +1,7 @@
+package Java24;
+
+public class PiTest {
+    public static void main(String[] args) {
+            System.out.printf("%f\n", Math.PI);
+    }
+}
---
+++ src/Java24/PiTest2.java
@@ -0,0 +1,31 @@
+package Java24;
+
+import java.util.Scanner;
+
+public class PiTest2 {
+public static void main(String[] args) {
+   Scanner in = new Scanner(System.in);
+   double n, i, j;     // Number of iterations and control variables
+   double f;           // factor that repeats
+   double pi = 1;
+      
+   System.out.printf("Approximation of the number PI through the Viete's series\n");
+   System.out.printf("\nEnter the number of iterations: ");
+   n = in.nextDouble();
+   System.out.printf("\nPlease wait. Running...\n");   
+    
+   for(i = n; i > 1; i--) {
+      f = 2;
+      for(j = 1; j < i; j++){
+         f = 2 + Math.sqrt(f);
+      }
+      f = Math.sqrt(f);
+      pi = pi * f / 2;
+   }
+   pi *= Math.sqrt(2) / 2;
+   pi = 2 / pi;
+   
+   System.out.printf("\nAproximated value of PI = %f\n", pi);  
+
+}
+}
