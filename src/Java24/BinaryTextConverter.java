9bc5196af205c693d73b5f1e44fbfb25ac6ea927
Jonathan <>
2017-12-09 20:47:50 +0000

    text to binary converter
    1110100 1101000 1100001 1110100 100111 1110011 100000 1110010 1100001 1100100

---
+++ src/Java24/BinaryTextConverter.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Java24;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jonathan
+ */
+public class BinaryTextConverter {
+    public static void displayBinary(int value) {
+        if (value > 0) {
+            displayBinary(value / 2);
+            System.out.print(value % 2);
+        }
+    }
+    private static void convertBinary(String input) {
+        int binary;
+        char[] process = input.toCharArray();
+        for (int dex = 0; dex < process.length; dex++) {
+            char current = process[dex];
+            binary = (int)current;
+            displayBinary(binary);
+            System.out.print(" ");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter number to be converted: ");
+        String input = in.nextLine();
+        convertBinary(input);
+        System.out.println();
+    }
+}
