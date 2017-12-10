e0894fbd1845aed27a29690834ccec5e3fe96e2c
Jonathan <>
2017-12-10 11:25:17 +0000

    I FIXED IT, BOOYAH

--- src/Java24/DecimalBinaryConverter.java
+++ src/Java24/DecimalBinaryConverter.java
@@ -12,16 +12,18 @@ import java.util.Scanner;
  * @author Jonathan
  */
 public class DecimalBinaryConverter {
+    //the general idea - The decimal number is equal to the sum of binary digits (dn) times their power of 2 (2n):
+    //decimal = d0×2^0 + d1×2^1 + d2×2^2 + ...
     public static void binaryConverter(String input) {
-        double output= 1.0;
+        double output= 0.0;
         //put input into an array
         char[] process = input.toCharArray();
-        //compute result
+        //compute result peice by peice
         for (int dex = 0; dex < process.length; dex++) {
             //set current value to int
             int current = process[dex];
             //multiply current value(either 0 or 1) by 2^(their position)
-            double progress = (current - 48) * Math.pow(2, (process.length - dex));
+            double progress = (current - 48) * Math.pow(2, (process.length - (dex+1)));
             //add the results together
             output = output + progress;
         }
