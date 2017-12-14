c1021f1a09a4fde28498303511b5afa49df78cc9
Jonathan <>
2017-12-14 21:17:00 +0000

    I DID MATH
    lots and lots of math

--- src/Java24/MyMath.java
+++ src/Java24/MyMath.java
@@ -14,6 +14,7 @@ public class MyMath {
     public static boolean isDivisible(int n, int m) {
         return n % m == 0;
     }
+    
     public static boolean isTriangle(int a, int b, int c) {
         if (a+b < c) {
             return false;
@@ -23,5 +24,46 @@ public class MyMath {
             return false;
         }
         return true;
+    }
+    
+    public static double multadd(double a, double b, double c) {
+        return a * b + c;
+    }
+    
+    public static boolean isEven(int a) {
+        return a % 2 == 0;
+    }
+    
+    public static double expSum(double x) {
+        return MyMath.multadd(x, Math.exp(-x), Math.sqrt(1-Math.exp(-x)));
+    }
+    
+    public static int ack(int m, int n) {
+        if (m == 0) {
+            return n+1;
+        } else if (m > 0 && n == 0) {
+            return ack(m-1, 1);
+        } else if (m> 0 && n > 0) {
+            return ack(m-1, ack(m, n-1));
+        }
+        return 0;
+    }
+    
+    public static boolean isPositive(double n) {
+        return n * -1 < 0;
+    }
+    
+    public static double pow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        } else if (isEven(n)) {
+            if (!isPositive(n)) {
+                return 1 / pow(x, -n);
+            }
+            return pow(x, n/2) * pow(x, n/2);
+        } if (!isPositive(n)) {
+        return 1 / pow(x, -n);
+        }
+        return x * pow(x, n-1);
     }
 }
--- src/Java24/test.java
+++ src/Java24/test.java
@@ -1,13 +1,28 @@
 package Java24;
 
-import static test.testinit.*;
+//import static test.testinit.*;
 class test{
+//    public static void main(String[] args) {
+//        System.out.println("My speed is " + speed + ", my age is " + age 
+//                + " and the time is " + time);
+//        //testing the thing
+//        double pi = Math.PI;
+//        System.out.println(pi);
+//    }
     public static void main(String[] args) {
-        System.out.println("My speed is " + speed + ", my age is " + age 
-                + " and the time is " + time);
-        //testing the thing
-        double pi = Math.PI;
-        System.out.println(pi);
+        double result = MyMath.multadd(1, 2, 3);
+        System.out.println(result);
+        
+        result = MyMath.multadd(1, Math.sin(Math.PI/4), Math.cos((Math.PI/4)/2));
+        System.out.println(result);
+        result = Math.sin(Math.PI/4) + Math.cos((Math.PI/4)/2);
+        System.out.println(result);
+        result = Math.log(10) + Math.log(20);
+        System.out.println(result);
+        System.out.println(MyMath.ack(3, 10));
+        System.out.print("Is my math good math: ");
+        if (MyMath.pow(-13, -10) == Math.pow(-13, -10)) {
+            System.out.println("Yes");
+        } else System.out.println("No");
     }
-    
 }
