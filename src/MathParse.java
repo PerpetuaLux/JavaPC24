48ff9e1df64d3c27edf8738dfb1a5d14d8f79fff
Jonathan <>
2017-12-19 15:56:08 +0000

    made it display your input
    (in a very complicated way that should help later)

--- src/MathParse.java
+++ src/MathParse.java
@@ -15,24 +15,41 @@ public class MathParse {
     static char current = 'a';
     static char lastin = 'a';
     static char nextin = 'a';
-    static int counting = 0;
+    static int counting = -1;
     static char[] process;
+    static String num = ("");
+    static int length;
+    static int position;
     public static void count() {
         counting++;
+        if (counting >= 0 && counting < length) {
         current = process[counting];
+        } else {
+            current = '@';
+        }
+        if (counting >= 1 && counting < length - 2) {
         lastin = process[counting - 1];
+        }
+        if (counting >= 0 && counting < length - 1) {
         nextin = process[counting + 1];
+        }
+    }
+    public static void num() {
+        
+        if (current >= 48 && current <= 57) {
+            num = (num + current);
+            count();
+            num();
+        }
     }
     
     
     public static void parseMethod(String input) {
         process = input.toCharArray();
-        current = process[counting];
-        nextin = process[counting + 1];
-        if (current >= 48 && current <= 57) {
-            String num = (current + "" + nextin);
-            System.out.println(num);
-        }
+        length = process.length;
+        count();
+        num();
+        System.out.println(num);
     }
     
     public static void main(String[] args) {
